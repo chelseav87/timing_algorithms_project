9c4fc053d0b763a3365f700051ac983de0807dd1
chelseav87 <chelseav87@kprschools.ca>
2026-04-27 14:21:42 -0400

    update

--- main.py
+++ main.py
@@ -1,81 +1,66 @@
-import random
+import timeit
 
-def print_array(arr):
-    for val in arr:
-        print(val, end=" ")
-    print()
-
-size = 10
-arr = [random.randint(0,100) for i in range(size)]
-array = [random.randint(0,100) for j in range(size)]
-
-
-# selection sort; iterative implementation
-
-def selection_sort(arr):
-    n = len(arr)
+def selection_sort(array):  # iterative implementation
+    n = len(array)
     for i in range(n - 1):
-
-        # Assume the current position holds the minimum element
         min_idx = i
 
-        # Iterate through the unsorted portion to find the actual minimum
         for j in range(i + 1, n):
-            if arr[j] < arr[min_idx]:
-                # Update min_idx if a smaller element is found
+            if array[j] < array[min_idx]:
                 min_idx = j
 
-        # Move minimum element to its correct position
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        array[i], array[min_idx] = array[min_idx], array[i]
 
-if __name__ == "__main__":
-    print("Original array: ", end="")
-    print_array(arr)
+def quick_sort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
 
-    selection_sort(arr)
+        quick_sort(array, low, pi - 1)
+        quick_sort(array, pi + 1, high)
 
-    print("Sorted array: ", end="")
-    print_array(arr)
-
-
-# quick sort; using lomuto partition algorithm
-
-def partition(array, low, high):
-    # choose the pivot
+def partition(array, low, high):    # lomuto partition algorithm
     pivot = array[high]
 
-    # index of smaller element and indicates the right position of pivot found so far
     i = low - 1
 
-    # traverse array and move all smaller elements to the left side. Elements from low to i are smaller after every iteration
     for j in range(low, high):
         if array[j] < pivot:
             i += 1
             swap(array, i, j)
 
-    # move pivot after smaller elements and return its position
     swap(array, i + 1, high)
     return i + 1
 
 def swap(array, i, j):
     array[i], array[j] = array[j], array[i]
 
-def quick_sort(array, low, high):
-    if low < high:
-        # pi is the partition return index of pivot
-        pi = partition(array, low, high)
+def test_algorithms(size, trials, repeats):
+    setup = """
+from __main__ import selection_sort, quick_sort
+import random
+array = array_2 = [random.randint(0, 100) for x in range(""" + str(size) + """)]
+n = len(array)
+print(f"Sorting: {array}")
+"""
 
-        # recursion calls for smaller elements and greater or equal elements
-        quick_sort(array, low, pi - 1)
-        quick_sort(array, pi + 1, high)
+    test_select = """
+selection_sort(array)
+print(f"Sorted: {array}")
+"""
+    test_quick = """
+quick_sort(array_2, 0, n - 1)
+print(f"Sorted: {array_2}")
+"""
 
-if __name__ == "__main__":
-    n = len(array)
+    select_times = timeit.repeat(stmt=test_select,setup=setup,repeat=repeats,number=trials)
+    select_min_time = min(select_times)
+    print(f"Selection sort minimum time: {select_min_time/trials:.20f}\n")
 
-    print("\nOriginal array: ", end="")
-    print_array(array)
+    quick_times = timeit.repeat(stmt=test_quick, setup=setup, repeat=repeats, number=trials)
+    quick_min_time = min(quick_times)
+    print(f"Quick sort minimum time: {quick_min_time / trials:.20f}")
 
-    quick_sort(array, 0, n - 1)
-
-    print("Sorted array: ", end="")
-    print_array(array)
+SIZE = 20
+TRIALS = 1
+REPEATS = 1
+test_algorithms(SIZE, TRIALS, REPEATS)
