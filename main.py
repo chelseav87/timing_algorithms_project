4bfd5a56c1a6779d821f23e4308eaafd7c0653c4
chelseav87 <chelseav87@kprschools.ca>
2026-04-28 14:15:03 -0400

    update

--- main.py
+++ main.py
@@ -36,26 +36,26 @@ def swap(array, i, j):
 
 import random
 
-unsorted_array = [random.randint(0, 100) for x in range(5000)]
+unsorted_array = [random.randint(0, 100) for x in range(100)]
 
 def test_algorithms(trials, repeats):
     setup = """
 from __main__ import selection_sort, quick_sort, unsorted_array
 import random, copy
 
-select_array = quick_array = copy.deepcopy(unsorted_array)
+select_array = quick_array = builtin_array = copy.deepcopy(unsorted_array)
 
 n = len(unsorted_array)
-#print(f"Sorting: {unsorted_array}")
+# print(f"Sorting: {unsorted_array}")
 """
 
     test_select = """
 selection_sort(select_array)
-#print(f"Sorted: {select_array}")
+# print(f"Sorted: {select_array}")
 """
     test_quick = """
-quick_sort(select_array, 0, n - 1)
-#print(f"Sorted: {select_array}")
+quick_sort(quick_array, 0, n - 1)
+# print(f"Sorted: {quick_array}")
 """
 
     select_times = timeit.repeat(stmt=test_select,setup=setup,repeat=repeats,number=trials)
